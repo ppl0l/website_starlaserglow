25a47739048bc3c351b863e46ce197a872693d9f
00__666 <153538262+ppl0l@users.noreply.github.com>
2026-01-21 01:21:36 +0300

    ll

--- src/components/Home/ContactPreview.js
+++ src/components/Home/ContactPreview.js
@@ -16,10 +16,10 @@ function ContactPreview() {
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
   
   const photos = [
-    "/photo/1.png",
-    "/photo/2.png", 
+    "/photo/4.png",
     "/photo/3.png",
-    "/photo/4.png"
+    "/photo/1.png",
+    "/photo/2.png"
   ];
   
   const handleCardHover = (ref, isHovering) => {
